a7dad669abf395e89e87159f7a1e52d8491e0bca
Andreas Linz <klingt.net@gmail.com>
2016-04-14 11:00:40 +0200

    Fix path in file check

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -2,7 +2,7 @@
 
 [ $(hash nvim) ] &&\
     echo "Creating symlinks to setup the neovim configuration ..."
-    [ ! -f "$HOME/.config/init.vim" ] &&\
+    [ ! -f "$HOME/.vim/init.vim" ] &&\
         ln -s $HOME/.vimrc $HOME/.vim/init.vim
     [ ! -d "$HOME/.config/nvim" ] &&\
         ln -s $HOME/.vim $HOME/.config/nvim
