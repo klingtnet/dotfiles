d36b67670a7eacd518b7268a2c9f2156170dbca1
Andreas Linz <klingt.net@gmail.com>
2018-06-19 00:43:06 +0200

    Return a Result type in main
    This is a stable feature since Rust 1.26 and simplifies error handling a
    bit.

--- tools/shell-timer/src/main.rs
+++ tools/shell-timer/src/main.rs
@@ -3,17 +3,17 @@
 extern crate humantime;
 
 use std::env;
+use std::num::ParseIntError;
 use std::time::Duration;
 
-fn main() {
-    let start = env::var("KN_CMD_START_TIME_NS")
+fn main() -> Result<(), ParseIntError> {
+    let start = try!(env::var("KN_CMD_START_TIME_NS")
         .unwrap_or_default()
-        .parse::<u64>()
-        .unwrap_or(0);
-    let end = env::var("KN_CMD_END_TIME_NS")
+        .parse::<u64>());
+    let end = try!(env::var("KN_CMD_END_TIME_NS")
         .unwrap_or_default()
-        .parse::<u64>()
-        .unwrap_or(0);
+        .parse::<u64>());
     let dur_str = humantime::format_duration(Duration::from_nanos(end - start)).to_string();
     println!("{}", dur_str);
+    Ok(())
 }
