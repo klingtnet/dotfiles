cc3b2f3aab1bfdaf3dc12ed2852e43212891f70f
Andreas Linz <klingt.net@gmail.com>
2019-01-31 22:16:04 +0100

    Set connect-timeout for public IP resolving to 1 second

--- tools/xfce4-genmon-panel.sh
+++ tools/xfce4-genmon-panel.sh
@@ -8,7 +8,7 @@ _HOST="$(hostname)"
 _WIFI="$(nmcli --terse --fields name connection show --active | tr ' ' _ | xargs | tr ' ' ,)"
 _UPTIME="$(uptime --pretty)"
 _IP="$(ip -json route show scope link | jq --raw-output '.[].prefsrc' | xargs | tr ' ' ,)"
-_PUBLIC_IP="$(curl -sf https://httpbin.org/ip | jq --raw-output .origin)"
+_PUBLIC_IP="$(curl --connect-timeout 1 --silent --fail https://httpbin.org/ip | jq --raw-output .origin)"
 _BAT="$(for bat in $(ls /sys/class/power_supply); do [[ -e "/sys/class/power_supply/${bat}/capacity" ]] && echo ${bat} $(cat "/sys/class/power_supply/${bat}/capacity")\%; done)"
 [[ -z ${_BAT} ]] && _BAT='AC'
 
