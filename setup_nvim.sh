df9de5200731b3d8550fd57ee2c1811d4b3f0caa
Andreas Linz <anli@spreadshirt.net>
2019-12-04 10:53:49 +0100

    Check if files are symbolic links
    Using -f will cause `ln -s` to fail since the destination already exists
    ,is a symbolic link already and not a file, hence the condition was
    true.

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -2,11 +2,11 @@
 
 [ $(hash nvim &>/dev/null) ] &&
     echo "Creating symlinks to setup the neovim configuration ..."
-    [ ! -f "$HOME/.vim/init.vim" ] &&
+    [ ! -L "$HOME/.vim/init.vim" ] &&
         ln -s $HOME/.vimrc $HOME/.vim/init.vim
-    [ ! -f "$HOME/.vim/ginit.vim" ] &&
+    [ ! -L "$HOME/.vim/ginit.vim" ] &&
         ln -s $HOME/.ginit.vim $HOME/.vim/ginit.vim
-    [ ! -d "$HOME/.config/nvim" ] &&
+    [ ! -L "$HOME/.config/nvim" ] &&
         ln -s $HOME/.vim $HOME/.config/nvim
 # reset possible error value if nvim wasn't found
 echo $? > /dev/null
